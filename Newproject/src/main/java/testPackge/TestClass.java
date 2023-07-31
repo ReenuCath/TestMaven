bc1f2651d68d8bfe5163a198daa35246580417ff
ThomasAnish <mail2anishthomas@gmail.com>
2023-07-31 12:21:45 +0530

    NA

---
+++ Newproject/src/main/java/testPackge/TestClass.java
@@ -0,0 +1,16 @@
+package testPackge;
+
+public class TestClass 
+{
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) 
+	{
+		TestClass obj=new TestClass();
+		obj.display();
+
+	}
+}
